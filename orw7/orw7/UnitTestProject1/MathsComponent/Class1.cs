5055a283d9bd9e3188f767442419ac5266c048c2
aki3l007 <akielewicz.mateusz@gmail.com>
2016-01-25 21:04:23 +0100

    laborki

---
+++ orw7/orw7/UnitTestProject1/MathsComponent/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsComponentNameSpace
+{
+    public class MathsComponent
+    {
+        public int Add(int num1, int num2) // zmienna publiczna 
+        {
+            return num1+num2; // zwróć dodawnaie
+        }
+
+        
+     }
+}
---
+++ orw7/orw7/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathsComponentNameSpace;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd() // publiczna klasa dodawania
+        {
+            MathsComponent obj = new MathsComponent(); // tworznie nowego obiektu
+            int result = obj.Add(2, 2); //inicjowanie dwóch zmiennych liczbowych
+            Assert.AreEqual(4, result); // wynik który ma wyjsc z testu
+        }
+    }
+}
